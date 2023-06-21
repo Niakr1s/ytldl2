ed9aab7d873f736a4a4e809144c7ed617638d211
Niakr1s <pavel2188@gmail.com>
2023-06-22 02:50:37 +0300

    made get_video_** public, renamed it, added tests

--- src/ytldl2/api.py
+++ src/ytldl2/api.py
@@ -18,6 +18,19 @@ class YtMusicApi:
         self._yt = YTMusic(auth=oauth)
         self._extractor = Extractor()
 
+    def get_home_items(self, home_limit: int = 100) -> HomeItems:
+        """
+        Gets home items from user's youtube music home page.
+        :param home_limit: Amount of items, requested from home items. Better to leave default.
+        """
+        try:
+            home = self._yt.get_home(limit=home_limit)
+            return self._extractor.parse_home(home)
+        except ExtractError:
+            raise
+        except Exception as e:
+            raise YtMusicApiError() from e
+
     def get_videos(
         self,
         home_items: HomeItems,
@@ -35,19 +48,6 @@ class YtMusicApi:
         except Exception as e:
             raise YtMusicApiError() from e
 
-    def get_home_items(self, home_limit: int = 100) -> HomeItems:
-        """
-        Gets home items from user's youtube music home page.
-        :param home_limit: Amount of items, requested from home items. Better to leave default.
-        """
-        try:
-            home = self._yt.get_home(limit=home_limit)
-            return self._extractor.parse_home(home)
-        except ExtractError:
-            raise
-        except Exception as e:
-            raise YtMusicApiError() from e
-
     def _get_videos(
         self, home_items: HomeItems, each_playlist_limit: int = 50
     ) -> list[Video]:
@@ -59,7 +59,7 @@ class YtMusicApi:
                 for playlist in playlists:
                     futures.append(
                         executor.submit(
-                            self._get_video_ids_from_playlist,
+                            self.get_videos_from_playlist,
                             playlist.playlistId,
                             limit=each_playlist_limit,
                         )
@@ -68,7 +68,7 @@ class YtMusicApi:
                 for channel in channels:
                     futures.append(
                         executor.submit(
-                            self._get_video_ids_from_channel,
+                            self.get_videos_from_channel,
                             channel.channelId,
                             limit=each_playlist_limit,
                         )
@@ -81,7 +81,7 @@ class YtMusicApi:
         print(f"Extracted {len(videos)} videos")
         return videos
 
-    def _get_video_ids_from_playlist(
+    def get_videos_from_playlist(
         self, playlist_id: PlaylistId, /, limit: int = 50
     ) -> list[Video]:
         """
@@ -93,13 +93,13 @@ class YtMusicApi:
             contents = self._yt.get_watch_playlist(playlistId=playlist_id, limit=limit)
         return self._extractor.extract_videos_from_playlist(contents)
 
-    def _get_video_ids_from_channel(
+    def get_videos_from_channel(
         self, channel_id: ChannelId, /, limit: int = 50
     ) -> list[Video]:
         """
         Extracts videoIds from channel.
         """
         artist = self._yt.get_artist(channelId=channel_id)
-        return self._get_video_ids_from_playlist(
+        return self.get_videos_from_playlist(
             self._extractor.extract_playlist_id_from_artist(artist), limit=limit
         )
