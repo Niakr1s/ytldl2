cab70c7ac76ef193c2a8249359dec5297efc4a71
Niakr1s <pavel2188@gmail.com>
2023-06-21 23:59:37 +0300

    simplified test_sqlite_cache

--- tests/ytldl2/__init__.py
+++ tests/ytldl2/__init__.py
@@ -12,10 +12,3 @@ TEST_CONFIG_DIR: pathlib.Path = pathlib.Path(
 TEST_CONFIG_DIR.mkdir(exist_ok=True)
 
 OAUTH_PATH = TEST_CONFIG_DIR / "oauth.json"
-
-
-def cp(file: Traversable, dir: pathlib.Path) -> pathlib.Path:
-    to_path = dir / file.name
-    with to_path.open("wb") as to:
-        to.write(file.read_bytes())
-    return to_path
--- tests/ytldl2/test_sqlite_cache.py
+++ tests/ytldl2/test_sqlite_cache.py
@@ -1,40 +1,25 @@
 import pathlib
 import sqlite3
+from copy import copy
+from datetime import datetime
 from time import sleep
 
 import pytest
 
-from tests.ytldl2 import DATA, cp
+from tests.ytldl2 import DATA
 from ytldl2 import VideoId
 from ytldl2.cache import CachedSongInfo
 from ytldl2.sqlite_cache import SqliteCache
 
 
 @pytest.fixture
-def non_exist_cache_path(tmp_path: pathlib.Path):
-    return str(tmp_path / "non_exist_cache.db")
+def invalid_cache_path():
+    return DATA / "invalid_cache.db"
 
 
 @pytest.fixture
-def invalid_cache_path(tmp_path):
-    return cp(DATA / "invalid_cache.db", tmp_path)
-
-
-@pytest.fixture
-def cache_path(tmp_path: pathlib.Path):
-    return cp(DATA / "cache.db", tmp_path)
-
-
-@pytest.fixture
-def cache(cache_path):
-    cache = SqliteCache(cache_path)
-    cache.open()
-    return cache
-
-
-@pytest.fixture
-def empty_valid_cache(non_exist_cache_path):
-    cache = SqliteCache(non_exist_cache_path)
+def cache(tmp_path: pathlib.Path):
+    cache = SqliteCache(tmp_path / "cache.db")
     cache.open()
     return cache
 
@@ -47,17 +32,7 @@ MOCK_SONG = CachedSongInfo(
 )
 
 
-STORED_SONG = CachedSongInfo(
-    VideoId("9khpyvTIBBc"),
-    "Ray",
-    "BUMP OF CHICKEN",
-    "some meaningful reason",
-)
-
-
-def test_open_non_exist_cache(non_exist_cache_path):
-    cache = SqliteCache(non_exist_cache_path)
-    cache.open()
+def test_empty_cache(cache):
     assert cache.db_version > 0
     assert len(cache) == 0
 
@@ -79,12 +54,6 @@ def test_close(cache: SqliteCache):
     assert set(video_ids) == set(new_video_ids)
 
 
-def test_valid_cache_has_stored_song(cache: SqliteCache):
-    got = cache[STORED_SONG.video_id]
-    assert got is not None
-    assert STORED_SONG == got
-
-
 def test_set_with_none_attribute(cache: SqliteCache):
     MOCK_SONG.filtered_reason = None
     cache.set(MOCK_SONG)
@@ -98,43 +67,40 @@ def test_set_with_empty_attribute(cache: SqliteCache):
 
 
 def test_get_set_len(cache: SqliteCache):
-    assert len(cache) == 1
+    assert len(cache) == 0
 
     cache.set(MOCK_SONG)
-    assert len(cache) == 2
+    assert len(cache) == 1
     assert MOCK_SONG.video_id in cache
     assert cache[MOCK_SONG.video_id] == MOCK_SONG
 
     cache.set(MOCK_SONG)
-    assert len(cache) == 2
+    assert len(cache) == 1
 
 
 def test_set_updates(cache: SqliteCache):
-    song = cache[STORED_SONG.video_id]
-    assert song is not None
-    NEW_ARTIST = "new artist"
-    song.artist = NEW_ARTIST
-    cache.set(song)
-    assert (song := cache[STORED_SONG.video_id]) and song.artist == NEW_ARTIST
+    cache.set(MOCK_SONG)
+
+    updated = copy(MOCK_SONG)
+    updated.artist = "new artist"
+    cache.set(updated)
+    assert (updated := cache[MOCK_SONG.video_id]) and updated.artist == updated.artist
 
 
 def test_iter(cache: SqliteCache):
-    song_to_set = MOCK_SONG
-    assert len(list(cache)) == 1
-    cache.set(song_to_set)
+    cache.set(MOCK_SONG)
     video_ids: list[VideoId] = list(cache)
-    assert len(video_ids) == 2
-    assert STORED_SONG.video_id in video_ids
-    assert song_to_set.video_id in video_ids
+    assert len(video_ids) == 1
+    assert MOCK_SONG.video_id in video_ids
 
 
 def test_last_modified(cache: SqliteCache):
-    cache.set(STORED_SONG)
-    last_modified_old = cache.last_modified(STORED_SONG.video_id)
+    cache.set(song=MOCK_SONG)
+    last_modified_old = cache.last_modified(MOCK_SONG.video_id)
     assert last_modified_old
 
     SLEEP_SECS = 0.1
     sleep(SLEEP_SECS)
-    cache.set(STORED_SONG)
-    diff = cache.last_modified(STORED_SONG.video_id) - last_modified_old
+    cache.set(MOCK_SONG)
+    diff = cache.last_modified(video_id=MOCK_SONG.video_id) - last_modified_old
     assert diff.total_seconds() >= SLEEP_SECS
