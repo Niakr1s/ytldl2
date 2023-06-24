edff5f81b6d42f1eb50e4b40d2f04a8bd839112c
Niakr1s <pavel2188@gmail.com>
2023-06-24 18:19:03 +0300

    moved test_get_infos from test_sqlitecache to test_cacahe

---
+++ tests/test_cache.py
@@ -0,0 +1,27 @@
+from ytldl2.cache import SongInfo
+from ytldl2.models import VideoId
+from ytldl2.sqlite_cache import SqliteCache
+
+
+class TestCache:
+    SONG_INFO = SongInfo(
+        id=VideoId("id"),
+        title="title",
+        duration=3,
+        channel="channel",
+        artist="artist",
+        lyrics="lyrics",
+    )
+
+    def test_get_infos(self):
+        cache = SqliteCache()
+        another_info = self.SONG_INFO.copy()
+        another_info.id = VideoId("another info")
+        ids = [self.SONG_INFO.id, another_info.id]
+        cache.set_info(self.SONG_INFO)
+        cache.set_info(another_info)
+
+        for expected in [ids, [*ids, "third id"]]:
+            assert set(expected) == set((got_infos := cache.get_infos(expected)).keys())
+            assert self.SONG_INFO.id in got_infos
+            assert another_info.id in got_infos
--- tests/ytldl2/test_sqlite_cache.py
+++ tests/ytldl2/test_sqlite_cache.py
@@ -132,15 +132,3 @@ class TestSqliteCache:
         cache.set_info(info)
         cached_info = cache.get_info(info.id)
         assert info == cached_info
-
-    def test_get_infos(self, cache: SqliteCache):
-        another_info = self.SONG_INFO.copy()
-        another_info.id = VideoId("another info")
-        ids = [self.SONG_INFO.id, another_info.id]
-        cache.set_info(self.SONG_INFO)
-        cache.set_info(another_info)
-
-        for expected in [ids, [*ids, "third id"]]:
-            assert set(expected) == set((got_infos := cache.get_infos(expected)).keys())
-            assert self.SONG_INFO.id in got_infos
-            assert another_info.id in got_infos
