37740b6504996eaea12ba91b9277493740a8bf1f
Niakr1s <pavel2188@gmail.com>
2023-06-24 08:36:24 +0300

    added tests to filter_home_items

--- tests/ytldl2/test_music_library.py
+++ tests/ytldl2/test_music_library.py
@@ -51,31 +51,61 @@ class TestMusicLibrary:
         assert (config_path := library.config.config_path).exists()
         assert config_path.read_text()
 
-    def test_filter_home_items(self):
-        video_titles = ["video 1", "video 2"]
-        playlist_titles = ["playlist 1", "playlist 2"]
-        channel_titles = ["channel 1", "channel 2"]
+    VIDEO_TITLES = ["video1", "video2"]
+    PLAYLIST_TITLES = ["playlist1", "playlist2"]
+    CHANNEL_TITLES = ["channel1", "channel2"]
 
-        home_items = HomeItems(
+    @pytest.fixture
+    def home_items(self) -> HomeItems:
+        return HomeItems(
             videos=[
                 Video(videoId=VideoId(str(id)), title=Title(title))
-                for id, title in enumerate(video_titles)
+                for id, title in enumerate(self.VIDEO_TITLES)
             ],
             playlists=[
                 Playlist(playlistId=PlaylistId(str(id)), title=Title(title))
-                for id, title in enumerate(playlist_titles)
+                for id, title in enumerate(self.PLAYLIST_TITLES)
             ],
             channels=[
                 Channel(browseId=BrowseId(str(id)), title=Title(title))
-                for id, title in enumerate(channel_titles)
+                for id, title in enumerate(self.CHANNEL_TITLES)
             ],
         )
 
-        filtered = MusicLibrary.filter_home_items(
-            home_items,
-            incl_videos=None,
-            incl_playlists=None,
-            incl_channels=None,
-        )
+    def test_filter_home_items__empty(self, home_items: HomeItems):
+        assert MusicLibrary.filter_home_items(home_items).is_empty()
 
-        assert filtered.is_empty()
+    def test_filter_home_items__unknown_titles(self, home_items: HomeItems):
+        home_items = MusicLibrary.filter_home_items(
+            home_items,
+            incl_videos=[Title("another video")],
+            incl_playlists=[Title("unknown playlist")],
+            incl_channels=[Title("unknown channel")],
+        )
+        assert not home_items.videos
+        assert not home_items.playlists
+        assert not home_items.channels
+
+    def test_filter_home_items__videos(self, home_items: HomeItems):
+        incl = Title(self.VIDEO_TITLES[0])
+        filtered = MusicLibrary.filter_home_items(home_items, incl_videos=[incl])
+        assert len(filtered.videos) == 1
+        assert len(filtered.playlists) == 0
+        assert len(filtered.channels) == 0
+        assert incl == filtered.videos[0].title
+
+    def test_filter_home_items__playlists(self, home_items: HomeItems):
+        incl = Title(self.PLAYLIST_TITLES[0])
+        filtered = MusicLibrary.filter_home_items(home_items, incl_playlists=[incl])
+        assert len(filtered.videos) == 0
+        assert len(filtered.playlists) == 1
+        assert len(filtered.channels) == 0
+        assert incl == filtered.playlists[0].title
+
+    def test_filter_home_items__channels(self, home_items: HomeItems):
+        incl = Title(self.CHANNEL_TITLES[0])
+        filtered = MusicLibrary.filter_home_items(home_items, incl_channels=[incl])
+        assert len(filtered.videos) == 0
+        assert len(filtered.playlists) == 0
+        assert len(filtered.channels) == 1
+        assert incl == filtered.channels[0].title
