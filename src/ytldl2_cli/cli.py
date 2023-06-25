91df7649385b742804589c52bffcca92e2e387bb
Niakr1s <pavel2188@gmail.com>
2023-06-25 22:42:43 +0300

    cli main stub

--- src/ytldl2_cli/cli.py
+++ src/ytldl2_cli/cli.py
@@ -1,5 +1,23 @@
+import logging
+import pathlib
+
+from ytldl2.cancellation_tokens import GracefulKiller
+from ytldl2.music_library import MusicLibrary
+
+logger = logging.getLogger()
+
+
 def main():
-    print("hello from cli")
+    home_dir = pathlib.Path("d:/!YT")
+    log_path = home_dir / ".logs" / "main.log"
+    log_path.parent.mkdir(parents=True, exist_ok=True)
+    log_path.touch()
+
+    logging.basicConfig(level=logging.INFO, filename=log_path)
+
+    cancellation_token = GracefulKiller()
+    lib = MusicLibrary(home_dir)
+    lib.update(limit=3, cancellation_token=cancellation_token)
 
 
 if __name__ == "__main__":
