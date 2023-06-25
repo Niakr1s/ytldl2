38ec2d491b1d4eda297583e0fd95d569b35f9362
Niakr1s <pavel2188@gmail.com>
2023-06-25 16:09:36 +0300

    removed doc from Video

--- src/ytldl2/models/video.py
+++ src/ytldl2/models/video.py
@@ -5,8 +5,4 @@ from ytldl2.models.types import Artist, WithTitle, WithVideoId
 
 @dataclass
 class Video(WithTitle, WithVideoId):
-    """
-    In raw home data, video is entity, that contains "video_id" field.
-    """
-
     artist: Artist | None = None
