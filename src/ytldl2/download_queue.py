e2b5259602bb55f60bd247a556e9716655c9f2ad
Niakr1s <pavel2188@gmail.com>
2023-06-26 10:18:19 +0300

    renamed _pending_mark to _current

--- src/ytldl2/download_queue.py
+++ src/ytldl2/download_queue.py
@@ -47,37 +47,37 @@ class DownloadQueue:
         self.skipped: list[Skipped] = []
         """List of items, skipped due to various reasons."""
 
-        self._pending_mark: VideoId | None = None
+        self._current: VideoId | None = None
         self._current_index = 0
 
     @property
-    def pending_mark(self) -> VideoId | None:
-        return self._pending_mark
+    def current(self) -> VideoId | None:
+        return self._current
 
     def revert(self) -> None:
         """Simply returns pending mark back to queue."""
-        if self._pending_mark is None:
+        if self._current is None:
             raise QueueError.no_pending()
-        self._pending_mark = None
+        self._current = None
         self._current_index -= 1
 
     def mark_downloaded(self, path: pathlib.Path) -> None:
-        if self._pending_mark is None:
+        if self._current is None:
             raise QueueError.no_pending()
-        self.downloaded.append(Downloaded(self._pending_mark, path))
-        self._pending_mark = None
+        self.downloaded.append(Downloaded(self._current, path))
+        self._current = None
 
     def mark_filtered(self, reason: str) -> None:
-        if self._pending_mark is None:
+        if self._current is None:
             raise QueueError.no_pending()
-        self.filtered.append(Filtered(self._pending_mark, reason))
-        self._pending_mark = None
+        self.filtered.append(Filtered(self._current, reason))
+        self._current = None
 
     def mark_skipped(self, reason: str) -> None:
-        if self._pending_mark is None:
+        if self._current is None:
             raise QueueError.no_pending()
-        self.skipped.append(Skipped(self._pending_mark, reason))
-        self._pending_mark = None
+        self.skipped.append(Skipped(self._current, reason))
+        self._current = None
 
     def __len__(self) -> int:
         return (
@@ -85,19 +85,19 @@ class DownloadQueue:
             - len(self.downloaded)
             - len(self.filtered)
             - len(self.skipped)
-            - (0 if self._pending_mark is None else 1)
+            - (0 if self._current is None else 1)
         )
 
     def __next__(self) -> VideoId:
         if self._current_index >= len(self.videos):
             raise StopIteration
 
-        if self._pending_mark is not None:
+        if self._current is not None:
             raise QueueError.has_pending()
 
-        self._pending_mark = self.videos[self._current_index]
+        self._current = self.videos[self._current_index]
         self._current_index += 1
-        return self._pending_mark
+        return self._current
 
     def __iter__(self) -> Iterator[VideoId]:
         return self
