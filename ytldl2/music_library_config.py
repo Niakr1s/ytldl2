be025512ba8d2de48428cfc97ad184833d7f609b
Niakr1s <pavel2188@gmail.com>
2026-02-08 13:25:19 +0300

    fixed your likes

--- ytldl2/music_library_config.py
+++ ytldl2/music_library_config.py
@@ -18,6 +18,7 @@ class MusicLibraryConfig(pydantic.BaseModel):
         return HomeItemsFilter(
             playlists=[
                 Title("Your Likes"),
+                Title("Liked Music"),
                 Title("My Supermix"),
                 Title("My Mix.*"),
             ],
